34b59a5dca3d11c3613f29aa2a2ce7f6997b3d61
Sayan Faraz <sayan96@hotmail.com>
2018-08-19 19:22:30 -0400

    adding starter code for p300 biosignal

---
+++ src/biosignals/p300.py
@@ -0,0 +1,12 @@
+from biosignals.biosignal import BioSignal
+
+class P300(BioSignal):
+
+    def __init__(self, sample_rate):
+        BioSignal.__init__(self)
+
+    def update(self, sample):
+        pass
+
+    def process(self):
+        pass
